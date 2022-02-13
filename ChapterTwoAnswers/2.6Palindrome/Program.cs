efa5e623b9e6ba5f3b5e005cdb560e3b5f1334f3
Noel Codes <no3lcodes@gmail.com>
2022-02-13 12:46:52 -0800

    Added new solutions

--- ChapterTwoAnswers/2.6Palindrome/Program.cs
+++ ChapterTwoAnswers/2.6Palindrome/Program.cs
@@ -1,35 +1,74 @@
 ﻿using SinglyLinkedListImplementation;
 
-bool IsLinkedListPalindrome(LinkedList ll)
-{
-    if (ll.Head == null) return false;
-
-    List<int> vals = new List<int>();
-    Node? node = ll.Head;
-
-    while (node != null)
-    {
-        vals.Add(node.Value);    
-        node = node.Next;
-    }
-
-    int left = 0;
-    int right = vals.Count - 1;
-    
-    while (left < right)
-    {
-        if (vals[left] != vals[right])
-        {
-            return false;
-        }
-
-        left++;
-        right--;
-    }
-
-    return true;
-}
+// 1) Solution using array
 
 // Time Complexity O(n)
 // Space Complexity O(n)
 
+//bool IsLinkedListPalindrome(LinkedList ll)
+//{
+//    if (ll.Head == null) return false;
+
+//    List<int> vals = new List<int>();
+//    Node? node = ll.Head;
+
+//    while (node != null)
+//    {
+//        vals.Add(node.Value);    
+//        node = node.Next;
+//    }
+
+//    int left = 0;
+//    int right = vals.Count - 1;
+
+//    while (left < right)
+//    {
+//        if (vals[left] != vals[right])
+//        {
+//            return false;
+//        }
+
+//        left++;
+//        right--;
+//    }
+
+//    return true;
+//}
+
+
+// 2) Solution using Stack (uses half memory and half time than solution using array);
+// Because we will only push half the elements in the linkedlist into the stacks and we will also compare those half elements
+bool isLinkedListPalindrome(LinkedList ll)
+{
+    if (ll.Head == null || ll.Head.Next == null) return false;
+
+    Stack<int> stack = new Stack<int>();
+    Node? fastPointer = ll.Head;
+    Node? slowPointer = ll.Head;
+
+    while (fastPointer != null && fastPointer.Next != null)
+    {
+        stack.Push(slowPointer.Value);
+        slowPointer = slowPointer.Next;
+        fastPointer = fastPointer.Next.Next;
+    }
+
+    if (fastPointer != null)
+    {
+        slowPointer = slowPointer.Next;
+    }
+
+    while (slowPointer != null)
+    {
+
+        int top = stack.Pop();
+        if (slowPointer.Value != top)
+        {
+
+            return false;
+        }
+        slowPointer = slowPointer.Next;
+    }
+
+    return true;
+}
